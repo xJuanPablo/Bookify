df4e8cbcc05ada5d6d60dff4ff9a880c1f570d63
Christopher Banta <chris.banta@rocketmail.com>
2023-08-15 11:26:11 -0500

    Add book to library function

--- controllers/api/library-routes.js
+++ controllers/api/library-routes.js
@@ -3,11 +3,13 @@ const { Library } = require('../../models')
 
 router.post('/', async (req,res) =>{
 try{
+  console.log('Incoming request body:', req.body);
   const libraryEntry = await Library.create({
     ...req.body
   })
   res.status(200).json(libraryEntry);
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
 });
