4d6c38a09f71832aeb3706d53bb973d7ffb421dc
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-15 18:21:00 -0500

    added doughnut chart to homepage.hbs

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,33 +1,15 @@
-// gsap.registerEffect({
-//     name: "fade",
-//     defaults: { duration: 10 }, //defaults get applied to the "config" object passed to the effect below
-//     effect: (targets, config) => {
-//         return gsap.to(targets, { duration: config.duration, opacity: 0 });
-//     }
-// });
-
-// //now we can use it like this:
-// //gsap.effects.fade("#myElement");
-
-// document.querySelectorAll("#myElement").forEach(function (element) {
-//     element.addEventListener("mouseenter", function () {
-//         gsap.effects.fade(this);
-//     });
-// });
-
-
-// Get the container
+// .content container animation w/ GSAP node package
 const items = document.querySelectorAll('.content');
 const text = "Bookify";
 const element = document.getElementById("myElement");
 
-// Split the text into individual characters
+// splits text into individ chars
 const chars = text.split("");
 
-// Set initial position of items outside the screen on the left
+// sets x-axis at -100% or off the screen to the left
 gsap.set(items, { x: '-100%' });
 
-// Animate the items into view
+// animates container box into view
 gsap.to(items, {
     duration: 4,
     x: '50%',
@@ -35,16 +17,16 @@ gsap.to(items, {
     opacity: 1,
     stagger: 0.1,
     ease: 'power2.out',
-    onComplete: function() {
+    onComplete: function () {
         console.log('Animation Completed!');
 
-        // Animate each character
+        // animates invidid chars
         chars.forEach((char, index) => {
             const charSpan = document.createElement("span");
             charSpan.textContent = char;
             charSpan.style.opacity = 0;
             element.appendChild(charSpan);
-        
+
             gsap.to(charSpan, {
                 duration: 0.5,
                 opacity: 1,
@@ -53,4 +35,72 @@ gsap.to(items, {
         });
     }
 });
+//===========================================================
+// doughtnut chart on homepage.hbs
+// used apex charts with help from flowbite
+window.addEventListener("load", function () {
+    const getChartOptions = () => {
+        return {
+            series: [90, 85, 70],
+            colors: ["#1C64F2", "#16BDCA", "#FDBA8C"],
+            chart: {
+                height: "380px",
+                width: "100%",
+                type: "radialBar",
+                sparkline: {
+                    enabled: true,
+                },
+            },
+            plotOptions: {
+                radialBar: {
+                    track: {
+                        background: '#E5E7EB',
+                    },
+                    dataLabels: {
+                        show: false,
+                    },
+                    hollow: {
+                        margin: 0,
+                        size: "32%",
+                    }
+                },
+            },
+            grid: {
+                show: false,
+                strokeDashArray: 4,
+                padding: {
+                    left: 2,
+                    right: 2,
+                    top: -23,
+                    bottom: -20,
+                },
+            },
+            labels: ["Done", "In progress", "To do"],
+            legend: {
+                show: true,
+                position: "bottom",
+                fontFamily: "Inter, sans-serif",
+            },
+            tooltip: {
+                enabled: true,
+                x: {
+                    show: false,
+                },
+            },
+            yaxis: {
+                show: false,
+                labels: {
+                    formatter: function (value) {
+                        return value + '%';
+                    }
+                }
+            }
+        }
+    }
 
+    if (document.getElementById("radial-chart") && typeof ApexCharts !== 'undefined') {
+        var chart = new ApexCharts(document.querySelector("#radial-chart"), getChartOptions());
+        chart.render();
+    }
+});
+//==============================================================
